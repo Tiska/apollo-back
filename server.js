14e089e6ce70c66ecd6fce1d7cfd68e77de34ec4
abody <adrien.body-prestataire@laposte.fr>
2017-08-04 19:53:25 +0200

    add cors for frontend

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress,graphiqlExpress } = require('apollo-server-express');
 const { schema } = require('./schema/index');
+const cors = require('cors');
 
 const PORT = 3000;
 const app = express();
+
+app.use('*', cors({ origin: 'http://localhost:3001' }));
 
 // bodyParser is needed just for POST.
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }));

a3a2adb352b2e376ed5c28eb8851b1a248644977
abody <tiska5576@gmail.com>
2017-08-12 22:21:31 +0200

    code cleaning

--- schema/mongodb-connector.js
+++ schema/mongodb-connector.js
@@ -1,7 +1,5 @@
 const Mongoose = require('mongoose');
 const ObjectId = (require('mongoose').Types.ObjectId);
-
-// somewhere in the middle:
 const mongo = Mongoose.connect('mongodb://localhost:27017/news');
 
 const NewsSchema = Mongoose.Schema({
@@ -12,5 +10,4 @@ const NewsSchema = Mongoose.Schema({
 
 const News = Mongoose.model('news', NewsSchema);
 
-// at the bottom, add View to the exports
 module.exports = { News };
--- schema/schema.js
+++ schema/schema.js
@@ -30,8 +30,6 @@ type Subscription {
   newsVoted: News
 }
 
-# we need to tell the server which types represent the root query
-# and root mutation types. We call them RootQuery and RootMutation by convention.
 schema {
   query: Query
   mutation: Mutation
